45d3ec3479d3947475a6103fdfadd0c4e9c5f8f7
Daria <tityaevadash@gmail.com>
2021-03-16 22:27:24 +0300

    Сверстан header c адаптивом

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,28 @@
-$('.header__burger').on('click', function(e) {
-    e.preventDefault;
-    $(this).toggleClass('header__burger_active');
-})
+(function ($) {
+    "use strict";
+    //=======бургер=====
+    $('.header__burger').on('click', function (e) {
+        e.preventDefault;
+        $(this).toggleClass('header__burger_active');
+        $('.header').toggleClass('menu-active');
+    })
+
+
+
+    //=======скролл наверх=====
+    let btn = $('.scroll__btn');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('scroll__btn_show');
+        } else {
+            btn.removeClass('scroll__btn_show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+    });
+
+})(jQuery);
