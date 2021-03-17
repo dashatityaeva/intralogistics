5cd776131ccd32253c3eab4f0d851a6c790d023f
Daria <tityaevadash@gmail.com>
2021-03-17 16:48:26 +0300

    Сверстала блок слайдер компаний без адаптива

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,13 +6,24 @@
         $(this).toggleClass('header__burger_active');
         $('.header').toggleClass('menu-active');
     })
+    //=======слайдеры=====
+    $('#slider1').slick({
+        infinite: false,
+        // autoplay: true,
+        autoplaySpeed: 1500,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        draggable: false,
+        waitForAnimate: false
 
+    });
 
 
     //=======скролл наверх=====
     let btn = $('.scroll__btn');
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(window).scrollTop() > 300) {
             btn.addClass('scroll__btn_show');
         } else {
@@ -20,9 +31,11 @@
         }
     });
 
-    btn.on('click', function(e) {
+    btn.on('click', function (e) {
         e.preventDefault();
-        $('html, body').animate({scrollTop:0}, '300');
+        $('html, body').animate({
+            scrollTop: 0
+        }, '300');
     });
 
 })(jQuery);
